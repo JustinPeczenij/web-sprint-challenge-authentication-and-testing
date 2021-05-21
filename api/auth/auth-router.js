61caebd96130a4859ab15a7d871882995732ee66
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-21 12:34:49 -0400

    login router is functional with mw and sends a token

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs')
 const Users = require('../users/users-model')
-const { checkBody } = require('../middleware/register') 
+const rmw = require('../middleware/register') 
+const lmw = require('../middleware/login')
+const tokenBuilder = require('./tokenBuilder') 
 
-router.post('/register', checkBody, async (req, res, next) => {
+router.post('/register', rmw.checkBody, async (req, res, next) => {
   try {
     let user = req.body
     const rounds = process.env.ROUNDS || 8 //production will want to hide the # of rounds
@@ -42,8 +44,18 @@ router.post('/register', checkBody, async (req, res, next) => {
   */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', lmw.checkBody, async (req, res, next) => {
+  try {
+    let { password } = req.body
+    if(req.user && bcrypt.compareSync(password, req.user.password)) {
+      const token = tokenBuilder(req.user)
+      res.status(200).json({ message: `welcome, ${req.user.username}`, token })
+    } else {
+      res.status(401).json({ message: "invalid credentials" })
+    }
+  } catch(err) {
+    next(err)
+  }
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
