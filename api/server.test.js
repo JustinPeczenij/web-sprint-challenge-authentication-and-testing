fb853f73c6b9987b12f6c96598f42d5060ab3fce
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-21 14:17:27 -0400

    created tests for endpoints

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,89 @@
-// Write your tests here
+const db = require('../data/dbConfig')
+const request = require('supertest')
+const server = require('./server')
+
+beforeAll(async () => {
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db.seed.run() 
+})
+
+afterAll(async () => {
+  await db.seed.run() 
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(process.env.NODE_ENV).toBe('testing')
 })
 
+describe('[POST] /api/auth/register', () => {
+  // beforeEach(async () => {
+  //   await db('users').insert({ username: 'bonto', password: '1234' })
+  // })
+  it('responds with "username and password required" when password is missing', async () => {
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'timmy'})
+    expect(res.body).toEqual({message: 'username and password required'})
+  })
+  it('responds with "username taken" when username is taken', async () => {
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'bonto', password: '1234'})
+    expect(res.body).toEqual({message: 'username taken'})
+  })
+})
+
+describe('[POST] /api/auth/login', () => {
+  // beforeEach(async () => {
+  //   await db('users').insert({ username: 'bonto', password: '1234' })
+  // })
+  it('responds with "invalid credentials" if username does not exist', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'tricky', password: '1234' })
+    expect(res.body).toEqual({ message: 'invalid credentials' })
+  })
+  it('responds with "invalid credentials" if password is incorrect', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'bonto', password: '123' })
+    expect(res.body).toEqual({ message: 'invalid credentials' })
+  })
+  it('responds with a token with valid credentials', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'bonto', password: '1234' })
+    expect(res.body).toHaveProperty('token')
+  })
+
+  // cant figure out res.body resolves to an error message. I'm able to login on postman and the I know the knex syntax is right.
+  // I ended up making a seed to login - but I still don't understand why that wasn't working
+  // it('responds with a token upon successful login', async () => {
+  //   const res = await request(server).post('/api/auth/login').send({ username: 'bonto', password: '1234' })
+  //   console.log(res.body)
+  //   expect(res.body).toHaveProperty('token')
+  // })
+
+})
+
+describe('[GET] /api/jokes', () => {
+  it("can't get jokes without a token", async () => {
+    const res = await request(server).get('/api/jokes')
+    expect(res.body).toEqual({ message: 'token required' })
+  })
+  it("resolves to an array of jokes when a valid token is in the Request Headers", async () => {
+    const loginRes = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'bonto', password: '1234' })
+    const token = loginRes.body.token
+    const jokeRes = await request(server)
+      .get('/api/jokes')
+      .set('Authorization', token)
+    expect(jokeRes.body).toHaveLength(3)
+  })
+
+})
---
+++ data/seeds/01-users.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, username: 'user1', password: '$2a$08$xgNYEvHsoJm0Kfbn7lxZiODfzAX7eZMACg4Ml.JpmjnObX1DG7jRe'}, //1234
+        {id: 2, username: 'bonto', password: '$2a$08$LADeY4BUnCtZpEXfPjaxoeZGtNhE6yQbJ/M6D.uP0X/RCoJSFofni'}, //1234
+      ]);
+    });
+};
